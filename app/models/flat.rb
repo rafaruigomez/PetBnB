9c30310a13e51182cabfaa9663cbcb86e83b5c3a
Rafa Ruigomez <rafaruigomez@gmail.com>
2020-09-08 12:28:34 +0200

    add_column Address to flat

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,4 +1,11 @@
 class Flat < ApplicationRecord
   belongs_to :user
   has_many :reservations
+
+  validate :price, presence: true
+  validate :name, presence: true
+  validate :description, presence: true
+  validate :location, presence: true
+  validate :address, presence: true
+
 end
