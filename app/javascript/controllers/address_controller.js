4c9c7b28ee99f85ed94accc2c6103ab737868a56
Haess Romani <hromaza@gmail.com>
2020-09-11 00:18:04 -0500

    Change location to match administrative region

--- app/javascript/controllers/address_controller.js
+++ app/javascript/controllers/address_controller.js
@@ -10,7 +10,7 @@ export default class extends Controller {
     const addressInput = document.getElementById("flat_address");
     if (addressInput) {
       places({ container: addressInput }).on('change', function resultSelected(e) {
-        document.querySelector('#flat_location').value = e.suggestion.city || '';
+        document.querySelector('#flat_location').value = e.suggestion.administrative || '';
       });
     }
   };
